1413d0f0aec778861f92ebf4d78db1bf7df9df12
Vincent Willats <Vincentwillats@gmail.com>
2022-01-19 13:25:35 +0800

    updated faction theme struct

--- server/factions.go
+++ server/factions.go
@@ -1,10 +1,16 @@
 package server
 
+type FactionTheme struct {
+	Primary    string `json:"primary"`
+	Secondary  string `json:"secondary"`
+	Background string `json:"background"`
+}
+
 type Faction struct {
-	ID       FactionID `json:"id"`
-	Label    string    `json:"label"`
-	ImageUrl string    `json:"imageUrl"`
-	Colour   string    `json:"colour"`
+	ID       FactionID     `json:"id" db:"id"`
+	Label    string        `json:"label" db:"label"`
+	ImageUrl string        `json:"imageUrl"`
+	Theme    *FactionTheme `json:"theme" db:"theme"`
 }
 
 type FactionAbilityType string
