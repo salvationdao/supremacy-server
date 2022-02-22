931b2c8ebe104b45b0d7b1a3154a0d9c4c24fcd7
owengiri20 <owengiri20@gmail.com>
2022-02-23 00:35:24 +0800

    streams mig

---
+++ server/db/migrations/000002_seed_streams.up.sql
@@ -0,0 +1,36 @@
+
+-- insert 'Ninja Office' stream
+INSERT INTO stream_list (host , "name" , url , stream_id , region , resolution, bit_rates_k_bits , user_max , users_now , active , status , latitude , longitude )
+VALUES (
+'http://staging-watch-local.supremacy.game:5080', 
+'Ninja Office',
+'ws://10.25.26.11:5080/WebRTCAppEE/websocket',
+'524280586954581049507513',
+'au-east',
+'1920x1080',
+5000,
+1000,
+100,
+true,
+'online', 
+'-33.9031982421875',
+'151.15179443359375'
+);
+
+-- insert 'Sydney' stream
+INSERT INTO stream_list (host , "name" , url , stream_id , region , resolution, bit_rates_k_bits , user_max , users_now , active , status , latitude , longitude )
+VALUES (
+'https://staging-watch.supremacy.game:5443', 
+'Sydney',
+'wss://staging-watch.supremacy.game:5443/WebRTCAppEE/websocket',
+'524280586954581049507513',
+'au-east',
+'1920x1080',
+5000,
+1000,
+100,
+true,
+'online', 
+'-33.9031982421875',
+'151.15179443359375'
+);
