bbf0ff2ce50d2ea9412357c436b4b48426dc60ad
Yong Tan <ultimatoonmaster@gmail.com>
2022-02-23 13:08:47 +0800

    Update image_url to point to blobs

--- server/db/migrations/000004_game_ability_images.up.sql
+++ server/db/migrations/000004_game_ability_images.up.sql
@@ -11,3 +11,23 @@ CREATE TABLE blobs (
     updated_at      TIMESTAMPTZ      NOT NULL DEFAULT NOW(),
     created_at      TIMESTAMPTZ      NOT NULL DEFAULT NOW()
 );
+
+UPDATE game_abilities
+SET image_url = '/api/blobs/dc713e47-4119-494a-a81b-8ac92cf3222b'
+WHERE label = 'AIRSTRIKE';
+
+UPDATE game_abilities
+SET image_url = '/api/blobs/8e0e1918-556c-4370-85f9-b8960fd19554'
+WHERE label = 'NUKE';
+
+UPDATE game_abilities
+SET image_url = '/api/blobs/5d0a0028-c074-4ab5-b46e-14d0ff07795d'
+WHERE label = 'REINFORCEMENTS';
+
+UPDATE game_abilities
+SET image_url = '/api/blobs/f40e90b7-1ea2-4a91-bf0f-feb052a019be'
+WHERE label = 'REPAIR';
+
+UPDATE game_abilities
+SET image_url = '/api/blobs/3b4ae24a-7ccb-4d3b-8d88-905b406da0e1'
+WHERE label = 'ROBOT DOGS';
