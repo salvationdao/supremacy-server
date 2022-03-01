a00897b161d50b0d11e37c2c22a16621c73fe113
lolivise <tom790924@gmail.com>
2022-03-01 21:35:57 +0800

    fix error

--- server/api/middleware.go
+++ server/api/middleware.go
@@ -22,32 +22,23 @@ const (
 
 // ErrorObject is used by the front end react-fetching-library
 type ErrorObject struct {
-	Message   string `json:"message"`
-	ErrorCode string `json:"error_code"`
+	Message string `json:"message"`
 }
 
 func WithError(next func(w http.ResponseWriter, r *http.Request) (int, error)) func(w http.ResponseWriter, r *http.Request) {
 	fn := func(w http.ResponseWriter, r *http.Request) {
 		contents, _ := ioutil.ReadAll(r.Body)
-
 		r.Body = ioutil.NopCloser(bytes.NewReader(contents))
-
 		code, err := next(w, r)
 		if err != nil {
 			terror.Echo(err)
-
-			errObj := &ErrorObject{
-				Message:   err.Error(),
-				ErrorCode: generateErrorCode(5),
-			}
-
+			errObj := &ErrorObject{Message: err.Error()}
 			jsonErr, err := json.Marshal(errObj)
 			if err != nil {
 				terror.Echo(err)
 				http.Error(w, `{"message":"JSON failed, please contact IT.","error_code":"00001"}`, code)
 				return
 			}
-
 			http.Error(w, string(jsonErr), code)
 			return
 		}
