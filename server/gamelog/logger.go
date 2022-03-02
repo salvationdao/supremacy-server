7a44e597b330578249c20111959ba2ae8e58706f
John Nguyen <jtnguyen236@gmail.com>
2022-03-02 15:08:38 +0800

    add line numbers to logs

--- server/gamelog/logger.go
+++ server/gamelog/logger.go
@@ -13,6 +13,7 @@ func New(environment, level string) {
 	log := log_helpers.LoggerInitZero(environment, level)
 	if environment == "production" || environment == "staging" {
 		logPtr := zerolog.New(os.Stdout)
+		logPtr = logPtr.With().Caller().Logger()
 		log = &logPtr
 	}
 	log.Info().Msg("zerolog initialised")
