9fe7b18e942ca4b24cf10d8e98c09a5bdf07ce63
John Nguyen <jtnguyen236@gmail.com>
2022-03-03 17:58:19 +0800

    logtastic

--- server/db/contract_rewards.go
+++ server/db/contract_rewards.go
@@ -1,13 +1,11 @@
 package db
 
 import (
-	"errors"
 	"fmt"
 	"server"
 	"server/gamelog"
 
 	"github.com/georgysavva/scany/pgxscan"
-	"github.com/jackc/pgx/v4"
 	"github.com/shopspring/decimal"
 	"golang.org/x/net/context"
 )
@@ -30,7 +28,7 @@ func MechMetadata(ctx context.Context, conn Conn, hash string) (server.UserID, s
 	return result.OwnedByID, result.FactionID, result.Name, err
 }
 func IsInsured(ctx context.Context, tx Conn, hash string) (bool, error) {
-	gamelog.GameLog.Debug().Str("fn", "IsInsured").Msg("db func")
+	gamelog.GameLog.Debug().Str("fn", "IsInsured").Str("hash", hash).Msg("db func")
 	var result bool
 	q := `SELECT is_insured FROM battle_war_machine_queues WHERE war_machine_hash = $1`
 	err := pgxscan.Get(ctx, tx, &result, q, hash)
@@ -40,13 +38,19 @@ func IsInsured(ctx context.Context, tx Conn, hash string) (bool, error) {
 	return result, nil
 }
 func IsDefaultWarMachine(ctx context.Context, tx Conn, hash string) bool {
-	gamelog.GameLog.Debug().Str("fn", "IsDefaultWarMachine").Msg("db func")
-	_, err := ContractRewardGet(ctx, tx, hash)
-	return errors.Is(err, pgx.ErrNoRows)
+	gamelog.GameLog.Debug().Str("fn", "IsDefaultWarMachine").Str("hash", hash).Msg("db func")
+	var count int
+	q := `SELECT count(war_machine_hash) FROM battle_war_machine_queues WHERE war_machine_hash = $1`
+	err := pgxscan.Get(ctx, tx, &count, q)
+	if err != nil {
+		gamelog.GameLog.Err(err).Msg("check if default war machine")
+		return false
+	}
+	return count <= 0
 }
 
 func ContractRewardGet(ctx context.Context, tx Conn, hash string) (decimal.Decimal, error) {
-	gamelog.GameLog.Debug().Str("fn", "ContractRewardGet").Msg("db func")
+	gamelog.GameLog.Debug().Str("fn", "ContractRewardGet").Str("hash", hash).Msg("db func")
 	resultStr := struct {
 		ContractReward string
 	}{}
@@ -62,7 +66,7 @@ func ContractRewardGet(ctx context.Context, tx Conn, hash string) (decimal.Decim
 	return result, nil
 }
 func ContractRewardInsert(ctx context.Context, tx Conn, battleID server.BattleID, reward decimal.Decimal, hash string) error {
-	gamelog.GameLog.Debug().Str("fn", "ContractRewardInsert").Msg("db func")
+	gamelog.GameLog.Debug().Str("fn", "ContractRewardInsert").Str("battle_id", battleID.String()).Msg("db func")
 	q := `INSERT INTO issued_contract_rewards (battle_id, reward, war_machine_hash) VALUES ($1, $2, $3);`
 	_, err := tx.Exec(ctx, q, battleID, reward, hash)
 	if err != nil {
