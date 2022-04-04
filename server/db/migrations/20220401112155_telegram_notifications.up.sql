b77829b8c95db1ead3b5cd1757522ecebf8f1e95
owengiri20 <owengiri20@gmail.com>
2022-04-04 17:52:47 +0800

    merge dev

--- server/db/migrations/20220401112155_telegram_notifications.up.sql
+++ server/db/migrations/20220401112155_telegram_notifications.up.sql
@@ -1,11 +1,3 @@
 ALTER TABLE telegram_notifications ADD COLUMN shortcode TEXT NOT NULL;
 ALTER TABLE telegram_notifications ADD COLUMN registered BOOLEAN NOT NULL DEFAULT FALSE;
 ALTER TABLE telegram_notifications ADD COLUMN telegram_id INT;
-
-
-
-ALTER TABLE battle_queue_notifications ADD COLUMN player_id UUID REFERENCES players(id);
-
-
-
-
