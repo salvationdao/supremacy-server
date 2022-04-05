4285244b683966cbfce3ae6826eb2c67383730b0
owengiri20 <owengiri20@gmail.com>
2022-04-05 12:48:40 +0800

    update join

--- server/db/migrations/20220401112155_telegram_notifications.up.sql
+++ server/db/migrations/20220401112155_telegram_notifications.up.sql
@@ -1,3 +1,4 @@
-ALTER TABLE telegram_notifications ADD COLUMN shortcode TEXT NOT NULL;
-ALTER TABLE telegram_notifications ADD COLUMN registered BOOLEAN NOT NULL DEFAULT FALSE;
-ALTER TABLE telegram_notifications ADD COLUMN telegram_id INT;
+ALTER TABLE telegram_notifications
+    ADD COLUMN shortcode TEXT NOT NULL,
+    ADD COLUMN registered BOOLEAN NOT NULL DEFAULT FALSE,
+    ADD COLUMN telegram_id INT;
--- server/telegram/telegram.go
+++ server/telegram/telegram.go
@@ -72,10 +72,15 @@ func (t *Telegram) RunTelegram(bot *tele.Bot) error {
 		notification, err := boiler.BattleQueueNotifications(
 			boiler.BattleQueueNotificationWhere.IsRefunded.EQ(false),
 			boiler.BattleQueueNotificationWhere.SentAt.IsNull(),
-			qm.InnerJoin("telegram_notifications tn on tn.id = battle_queue_notifications.telegram_notification_id"),
-			qm.Where("tn.registered = false"),
-			qm.Where("tn.shortcode = ?", strings.ToLower(shortcode)),
+			// join battle queue notification with telegram notifications
+			qm.InnerJoin(fmt.Sprintf("%[1]s ON %[1]s.%[2]s = %[3]s.%[4]s ",
+				boiler.TableNames.TelegramNotifications,
+				boiler.TelegramNotificationColumns.ID,
+				boiler.TableNames.BattleQueueNotifications,
+				boiler.BattleQueueNotificationColumns.TelegramNotificationID)),
 
+			boiler.TelegramNotificationWhere.Registered.EQ(false),
+			boiler.TelegramNotificationWhere.Shortcode.EQ(strings.ToLower(shortcode)),
 			// load mech, telegramNotification rels
 			qm.Load(boiler.BattleQueueNotificationRels.TelegramNotification),
 			qm.Load(boiler.BattleQueueNotificationRels.Mech)).One(gamedb.StdConn)
