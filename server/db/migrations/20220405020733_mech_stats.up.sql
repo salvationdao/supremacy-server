e0ba0d778cecee10e9ca2c8b7fdd5b3f17de106b
felix t <41817193+o2bomb@users.noreply.github.com>
2022-04-05 17:22:27 +0800

    Remove begin commit

--- server/db/migrations/20220405020733_mech_stats.up.sql
+++ server/db/migrations/20220405020733_mech_stats.up.sql
@@ -1,10 +1,6 @@
-BEGIN;
-
 CREATE TABLE mech_stats (
     mech_id UUID PRIMARY KEY NOT NULL REFERENCES mechs (id),
     total_wins INT4 NOT NULL DEFAULT 0,
     total_deaths INT4 NOT NULL DEFAULT 0,
     total_kills INT4 NOT NULL DEFAULT 0
 );
-
-COMMIT;
