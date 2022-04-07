d91189202cabd5f980547307437cdb1467bf4f30
Alex Dunmow <alex@ninjasoftware.com.au>
2022-04-08 00:38:00 +0800

    remove contrib check

--- server/api/rest_check.go
+++ server/api/rest_check.go
@@ -2,21 +2,12 @@ package api
 
 import (
 	"context"
-	"database/sql"
-	"errors"
-	"fmt"
+	"github.com/go-chi/chi"
+	"github.com/rs/zerolog"
 	"net/http"
 	"server"
 	"server/battle"
 	"server/db"
-	"server/db/boiler"
-	"server/gamedb"
-	"time"
-
-	"github.com/volatiletech/sqlboiler/v4/queries/qm"
-
-	"github.com/go-chi/chi"
-	"github.com/rs/zerolog"
 )
 
 // CheckController holds connection data for handlers
@@ -43,7 +34,6 @@ func CheckRouter(log *zerolog.Logger, conn db.Conn, battleArena *battle.Arena, t
 }
 
 func (c *CheckController) Check(w http.ResponseWriter, r *http.Request) {
-	ok := true
 	err := check(context.Background(), c.Conn)
 	if err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
@@ -54,55 +44,54 @@ func (c *CheckController) Check(w http.ResponseWriter, r *http.Request) {
 		}
 	}
 
-	// get current battle
-	ba, err := boiler.Battles(qm.OrderBy(fmt.Sprintf("%s DESC", boiler.BattleColumns.BattleNumber)), qm.Limit(1)).One(gamedb.StdConn)
+	//// get current battle
+	//ba, err := boiler.Battles(qm.OrderBy(fmt.Sprintf("%s DESC", boiler.BattleColumns.BattleNumber)), qm.Limit(1)).One(gamedb.StdConn)
+	//if err != nil {
+	//	c.Log.Err(err).Msg("failed to retrieve battle")
+	//	return
+	//}
+
+	//now := time.Now()
+	//diff := now.Sub(ba.StartedAt)
+	//
+	//// check if current battle over 15 mins
+	//if diff.Minutes() > 15 {
+	//	ok = false
+	//	w.WriteHeader(http.StatusGone)
+	//	msg := fmt.Sprintf("current battle over 15 mins, battle started at: %s (%f mins ago)",
+	//		ba.StartedAt.String(),
+	//		diff.Minutes())
+	//
+	//	c.Log.Err(err).Str("battle_no", fmt.Sprintf("%d", ba.BattleNumber)).Msg(msg)
+	//	_, err = w.Write([]byte(msg))
+	//	if err != nil {
+	//		c.Log.Err(err).Str("battle_no", fmt.Sprintf("%d", ba.BattleNumber)).Msg("failed to send")
+	//	}
+	//}
+	//
+	//// get contributions for the last  2 mins
+	//btlContributions, err := ba.BattleContributions(boiler.BattleContributionWhere.ContributedAt.GT(now.Add(-10 * time.Minute))).All(gamedb.StdConn)
+	//if err != nil && !errors.Is(err, sql.ErrNoRows) {
+	//	c.Log.Err(err).Str("battle_no", fmt.Sprintf("%d", ba.BattleNumber)).Msg("failed to get battle contributions")
+	//
+	//}
+	//
+	//if len(btlContributions) <= 0 {
+	//	ok = false
+	//	w.WriteHeader(http.StatusGone)
+	//	msg := "there has been no contributions on the last 10 mins"
+	//	c.Log.Err(err).Str("battle_no", fmt.Sprintf("%d", ba.BattleNumber)).Msg(msg)
+	//	_, err = w.Write([]byte("\n" + msg))
+	//	if err != nil {
+	//		c.Log.Err(err).Str("battle_no", fmt.Sprintf("%d", ba.BattleNumber)).Msg("failed to send")
+	//	}
+	//}
+
+	_, err = w.Write([]byte("\nok"))
 	if err != nil {
-		c.Log.Err(err).Msg("failed to retrieve battle")
-		return
+		c.Log.Err(err).Msg("failed to send")
 	}
 
-	now := time.Now()
-	diff := now.Sub(ba.StartedAt)
-
-	// check if current battle over 15 mins
-	if diff.Minutes() > 15 {
-		ok = false
-		w.WriteHeader(http.StatusGone)
-		msg := fmt.Sprintf("current battle over 15 mins, battle started at: %s (%f mins ago)",
-			ba.StartedAt.String(),
-			diff.Minutes())
-
-		c.Log.Err(err).Str("battle_no", fmt.Sprintf("%d", ba.BattleNumber)).Msg(msg)
-		_, err = w.Write([]byte(msg))
-		if err != nil {
-			c.Log.Err(err).Str("battle_no", fmt.Sprintf("%d", ba.BattleNumber)).Msg("failed to send")
-		}
-	}
-
-	// get contributions for the last  2 mins
-	btlContributions, err := ba.BattleContributions(boiler.BattleContributionWhere.ContributedAt.GT(now.Add(-10 * time.Minute))).All(gamedb.StdConn)
-	if err != nil && !errors.Is(err, sql.ErrNoRows) {
-		c.Log.Err(err).Str("battle_no", fmt.Sprintf("%d", ba.BattleNumber)).Msg("failed to get battle contributions")
-
-	}
-
-	if len(btlContributions) <= 0 {
-		ok = false
-		w.WriteHeader(http.StatusGone)
-		msg := "there has been no contributions on the last 10 mins"
-		c.Log.Err(err).Str("battle_no", fmt.Sprintf("%d", ba.BattleNumber)).Msg(msg)
-		_, err = w.Write([]byte("\n" + msg))
-		if err != nil {
-			c.Log.Err(err).Str("battle_no", fmt.Sprintf("%d", ba.BattleNumber)).Msg("failed to send")
-		}
-	}
-
-	if ok {
-		_, err = w.Write([]byte("\nok"))
-		if err != nil {
-			c.Log.Err(err).Msg("failed to send")
-		}
-	}
 }
 
 // CheckGame return a game stat check
