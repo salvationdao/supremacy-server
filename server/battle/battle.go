bd8b23d600d70a638972441cdae3a56854970f76
Vinnie <vincent@weareninja.com>
2022-04-08 19:31:11 +0800

    fixed emoji

--- server/battle/battle.go
+++ server/battle/battle.go
@@ -1266,7 +1266,7 @@ func (btl *Battle) Destroyed(dp *BattleWMDestroyedPayload) {
 					} else {
 						if bqn.TelegramNotificationID.Valid {
 							// killed a war machine
-							msg := fmt.Sprintf("Your War machine destoryed %s 料 ", destroyedWarMachine.Name)
+							msg := fmt.Sprintf("Your War machine destoryed %s \U0001F9BE ", destroyedWarMachine.Name)
 							err := btl.arena.telegram.Notify(bqn.TelegramNotificationID.String, msg)
 							if err != nil {
 								gamelog.L.Error().Str("bqn.TelegramNotificationID.String", bqn.TelegramNotificationID.String).Err(err).Msg("failed to send notification")
