cd9de9ad7074ee2b133ecfbc5eeca0cc40c98d54
felix t <41817193+o2bomb@users.noreply.github.com>
2022-04-08 16:16:21 +0800

    Migration typo fix

--- server/db/migrations/20220408071310_mech_stats_fix.up.sql
+++ server/db/migrations/20220408071310_mech_stats_fix.up.sql
@@ -13,7 +13,7 @@ insert into mech_stats(mech_id, total_kills, total_deaths, battles_survived, tot
         coalesce((select count(_bh.war_machine_one_id) as deaths from battle_history _bh where _bh.war_machine_one_id = m.id group by _bh.war_machine_one_id), 0) as deaths,
         coalesce((select count(_bw.mech_id) from battle_wins _bw where _bw.mech_id = m.id group by _bw.mech_id), 0) as battles_survived,
         coalesce((select count(_bm.mech_id) from battle_mechs _bm where _bm.mech_id = m.id and _bm.faction_won group by _bm.mech_id), 0) faction_wins,
-        coalesce((select count(_bm.mech_id) from battle_mechs _bm where _bm.mech_id = m.id and not _bm.faction_won group by _bm.mech_id), 0) faction_losses,
+        coalesce((select count(_bm.mech_id) from battle_mechs _bm where _bm.mech_id = m.id and not _bm.faction_won group by _bm.mech_id), 0) faction_losses
     from mechs m
     left join battle_kills bk on bk.mech_id = m.id
     left join battle_wins bw on bw.mech_id = m.id
