d7f05f22464309253a5f623483a8fc40c38a9409
felix t <41817193+o2bomb@users.noreply.github.com>
2022-04-19 17:35:14 +0800

    Comment

--- server/hub_keys.go
+++ server/hub_keys.go
@@ -2,6 +2,7 @@ package server
 
 import "github.com/ninja-syndicate/hub"
 
+// player_abilities
 const HubKeyPlayerAbilitySubscribe = hub.HubCommandKey("PLAYER:ABILITY:SUBSCRIBE")
 const HubKeySaleAbilityPriceSubscribe = hub.HubCommandKey("SALE:ABILITY:PRICE:SUBSCRIBE")
 const HubKeyPlayerAbilitiesList = hub.HubCommandKey("PLAYER:ABILITIES:LIST")
