19996a3ef03c07b6e31b38f3a97c4fa4921d99c3
Vincent Willats <vincentwillats@gmail.com>
2022-05-05 16:56:50 +0800

    fix for yong

--- server/api/rest_check.go
+++ server/api/rest_check.go
@@ -57,52 +57,11 @@ func (c *CheckController) Check(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	//// get current battle
-	//ba, err := boiler.Battles(qm.OrderBy(fmt.Sprintf("%s DESC", boiler.BattleColumns.BattleNumber)), qm.Limit(1)).One(gamedb.StdConn)
-	//if err != nil {
-	//	gamelog.L.Err(err).Msg("failed to retrieve battle")
-	//	return
-	//}
-
-	//now := time.Now()
-	//diff := now.Sub(ba.StartedAt)
-	//
-	//// check if current battle over 15 mins
-	//if diff.Minutes() > 15 {
-	//	ok = false
-	//	w.WriteHeader(http.StatusGone)
-	//	msg := fmt.Sprintf("current battle over 15 mins, battle started at: %s (%f mins ago)",
-	//		ba.StartedAt.String(),
-	//		diff.Minutes())
-	//
-	//	gamelog.L.Err(err).Str("battle_no", fmt.Sprintf("%d", ba.BattleNumber)).Msg(msg)
-	//	_, err = w.Write([]byte(msg))
-	//	if err != nil {
-	//		gamelog.L.Err(err).Str("battle_no", fmt.Sprintf("%d", ba.BattleNumber)).Msg("failed to send")
-	//	}
-	//}
-	//
-	//// get contributions for the last  2 mins
-	//btlContributions, err := ba.BattleContributions(boiler.BattleContributionWhere.ContributedAt.GT(now.Add(-10 * time.Minute))).All(gamedb.StdConn)
-	//if err != nil && !errors.Is(err, sql.ErrNoRows) {
-	//	gamelog.L.Err(err).Str("battle_no", fmt.Sprintf("%d", ba.BattleNumber)).Msg("failed to get battle contributions")
-	//
-	//}
-	//
-	//if len(btlContributions) <= 0 {
-	//	ok = false
-	//	w.WriteHeader(http.StatusGone)
-	//	msg := "there has been no contributions on the last 10 mins"
-	//	gamelog.L.Err(err).Str("battle_no", fmt.Sprintf("%d", ba.BattleNumber)).Msg(msg)
-	//	_, err = w.Write([]byte("\n" + msg))
-	//	if err != nil {
-	//		gamelog.L.Err(err).Str("battle_no", fmt.Sprintf("%d", ba.BattleNumber)).Msg("failed to send")
-	//	}
-	//}
-
 	_, err = w.Write([]byte("\nok"))
 	if err != nil {
 		gamelog.L.Err(err).Msg("failed to send")
+		DatadogTracer.HttpFinishSpan(r.Context(), http.StatusInternalServerError, err)
+		return
 	}
 	DatadogTracer.HttpFinishSpan(r.Context(), http.StatusOK, nil)
 }
@@ -128,6 +87,7 @@ func (c *CheckController) CheckGameConnection(w http.ResponseWriter, r *http.Req
 	if err != nil {
 		gamelog.L.Err(err).Msg("failed to send")
 		DatadogTracer.HttpFinishSpan(r.Context(), http.StatusInternalServerError, err)
+		return
 	}
 	DatadogTracer.HttpFinishSpan(r.Context(), http.StatusOK, nil)
 }
@@ -175,6 +135,7 @@ func (c *CheckController) CheckGameLength(w http.ResponseWriter, r *http.Request
 	if err != nil {
 		gamelog.L.Err(err).Msg("failed to send")
 		DatadogTracer.HttpFinishSpan(r.Context(), http.StatusInternalServerError, err)
+		return
 	}
 	DatadogTracer.HttpFinishSpan(r.Context(), http.StatusOK, nil)
 }
@@ -240,6 +201,7 @@ func (c *CheckController) CheckGameContributes(w http.ResponseWriter, r *http.Re
 	if err != nil {
 		gamelog.L.Err(err).Msg("failed to send")
 		DatadogTracer.HttpFinishSpan(r.Context(), http.StatusInternalServerError, err)
+		return
 	}
 	DatadogTracer.HttpFinishSpan(r.Context(), http.StatusOK, nil)
 }
