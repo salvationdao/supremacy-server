416b7ef5846d216c34e67eabf2fadfabc9290f9e
alyssa <aruefenacht14@gmail.com>
2022-05-20 10:50:53 +0800

    seeding storefront

--- server/db/migrations/20220519061344_mystery_crate.up.sql
+++ server/db/migrations/20220519061344_mystery_crate.up.sql
@@ -7,7 +7,7 @@ CREATE TABLE storefront_mystery_crates
     mystery_crate_type CRATE_TYPE_ENUM NOT NULL,
     price              numeric(28)     NOT NULL,
     amount             INT             NOT NULL,
-    amount_sold        INT             NOT NULL,
+    amount_sold        INT             NOT NULL DEFAULT 0,
     faction_id         UUID            NOT NULL,
     deleted_at         TIMESTAMPTZ,
     updated_at         TIMESTAMPTZ     NOT NULL DEFAULT NOW(),
@@ -34,7 +34,7 @@ CREATE TABLE mystery_crate_blueprints
 );
 
 -- seeding mystery crates
--- loop over these for x amount of crates?
+-- looping over each type of mystery crate type for x amount of crates for each faction. can do 1 big loop if all crate types have the same amount
 do $$
 BEGIN
 FOR COUNT IN 1..10 loop
@@ -65,7 +65,19 @@ END LOOP;
 END;
 $$;
 
--- seeding blue prints
+-- seeding blueprints
+
 
 --seeding storefront
---
+-- for each faction, seed each type of crate and find how much are for sale
+DO $$
+DECLARE faction factions%rowtype;
+BEGIN
+FOR faction in SELECT * FROM factions
+LOOP
+INSERT INTO storefront_mystery_crates (mystery_crate_type, amount, faction_id, price) VALUES ('MECH', (SELECT COUNT(*) FROM mystery_crate WHERE type='MECH' AND faction_id=faction.id), faction.id, 500);
+INSERT INTO storefront_mystery_crates (mystery_crate_type, amount, faction_id, price) VALUES ('WEAPON', (SELECT COUNT(*) FROM mystery_crate WHERE type='MECH' AND faction_id=faction.id), faction.id, 500);
+INSERT INTO storefront_mystery_crates (mystery_crate_type, amount, faction_id, price) VALUES ('UTILITY', (SELECT COUNT(*) FROM mystery_crate WHERE type='MECH' AND faction_id=faction.id), faction.id, 500);
+END LOOP;
+END;
+$$;
