1a68604ee78d15c13a58ece3c185f23126bf1928
Vincent Willats <vincentwillats@gmail.com>
2022-05-31 12:06:13 +0800

    removed ghost mechs

---
+++ server/db/migrations/20220502135433_remove_ghost_mechs.up.sql
@@ -0,0 +1,13 @@
+DELETE
+FROM mech_stats
+WHERE mech_id = 'c120db1d-ca1c-4d03-b73c-b16bc5eb08cb';
+DELETE
+FROM mechs
+WHERE id = 'c120db1d-ca1c-4d03-b73c-b16bc5eb08cb';
+
+DELETE
+FROM mech_stats
+WHERE mech_id = '615e1e0b-6604-4d6f-8b3c-a161bf4e4558';
+DELETE
+FROM mechs
+WHERE id = '615e1e0b-6604-4d6f-8b3c-a161bf4e4558';
