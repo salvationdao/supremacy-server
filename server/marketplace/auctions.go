1c8ca150e39051f5b326134ae6090de8b60d151d
Yong Tan <ultimatoonmaster@gmail.com>
2022-06-01 16:51:51 +0800

    Add ticker system to deal with completed auctions

--- server/marketplace/auctions.go
+++ server/marketplace/auctions.go
@@ -3,11 +3,15 @@ package marketplace
 import (
 	"fmt"
 	"server/api"
+	"server/db"
 	"server/db/boiler"
 	"server/gamedb"
 	"server/gamelog"
 	"time"
 
+	"github.com/gofrs/uuid"
+	"github.com/volatiletech/null/v8"
+	"github.com/volatiletech/sqlboiler/v4/boil"
 	"github.com/volatiletech/sqlboiler/v4/queries/qm"
 )
 
@@ -16,8 +20,9 @@ type AuctionController struct {
 }
 
 type ItemSaleAuction struct {
-	ID               string `boil:"id"`
+	boiler.ItemSale  `boil:",bind"`
 	AuctionBidUserID string `boil:"auction_bid_user_id"`
+	AuctionBidTxID   string `boil:"auction_bid_tx_id"`
 	AuctionBidPrice  string `boil:"auction_bid_price"`
 }
 
@@ -39,16 +44,19 @@ func (a *AuctionController) Run() {
 			err := boiler.NewQuery(
 				qm.SQL(`
 					SELECT item_sales.id AS id,
-						MAX(item_sales_bid_history.bid_price) AS auction_bid_price,
+						item_sales.item_id,
+						item_sales.
+						item_sales_bid_history.bid_price AS auction_bid_price,
+						item_sales_bid_history.bid_tx_id AS auction_bid_tx_id,
 						item_sales_bid_history.bidder_id AS auction_bid_user_id
 					FROM item_sales 
 						INNER JOIN item_sales_bid_history ON item_sales_bid_history.item_sale_id = item_sales.id
-							AND item_sales_bid_history.cancelled_at is null
+							AND item_sales_bid_history.cancelled_at IS NULL
+							AND item_sales_bid_history.refund_bid_tx_id IS NOT NULL
 					WHERE item_sales.auction = TRUE
 						AND item_sales.sold_by IS NOT NULL
 						AND item_sales.end_at <= NOW()
-					GROUP BY item_sales.id, item_sales_bid_history.bidder_id
-					HAVING MAX(item_sales_bid_history.bid_price) >= auction_reserved_price ;
+						AND item_sales_bid_history.bid_price >= item_sales.auction_reserved_price
 				`),
 			).Bind(nil, gamedb.StdConn, &completedAuctions)
 			if err != nil {
@@ -59,30 +67,46 @@ func (a *AuctionController) Run() {
 				gamelog.L.Info().Int("num_pending", len(completedAuctions)).Msg("# completed auctions pending")
 			}
 
-			// Take Payment for Auction Items
 			for _, auctionItem := range completedAuctions {
-				fmt.Println("TODO", auctionItem)
-				// bidderUserID, err := uuid.FromString(auctionItem.AuctionBidUserID)
-				// if err != nil {
-				// 	gamelog.L.Error().
-				// 		Str("user_id", auctionItem.AuctionBidUserID).
-				// 		Str("item_id", auctionItem.ID).
-				// 		Err(err).
-				// 		Msg("Unable to get winning auction bid price.")
-				// 	continue
-				// }
+				itemSaleID, err := uuid.FromString(auctionItem.ID)
+				if err != nil {
+					gamelog.L.Error().
+						Str("item_sale_id", auctionItem.ID).
+						Err(err).
+						Msg("Failed to parse Item Sale ID.")
+					continue
+				}
 
-				// saleItemCost, err := decimal.NewFromString(auctionItem.AuctionBidPrice)
-				// if err != nil {
-				// 	gamelog.L.Error().
-				// 		Str("user_id", auctionItem.AuctionBidUserID).
-				// 		Str("item_id", auctionItem.ID).
-				// 		Err(err).
-				// 		Msg("Unable to get winning auction bid price.")
-				// 	continue
-				// }
+				// Update Item Sale Record
+				saleItemRecord := auctionItem.ItemSale
+				saleItemRecord.SoldAt = null.TimeFrom(time.Now())
+				saleItemRecord.SoldFor = null.StringFrom(auctionItem.AuctionBidPrice)
+				saleItemRecord.SoldTXID = null.StringFrom(auctionItem.AuctionBidTxID)
+				saleItemRecord.SoldBy = null.StringFrom(auctionItem.AuctionBidUserID)
 
-				// userBalance
+				_, err = saleItemRecord.Update(gamedb.StdConn, boil.Infer())
+				if err != nil {
+					err = fmt.Errorf("failed to complete payment transaction")
+					gamelog.L.Error().
+						Str("item_id", auctionItem.ID).
+						Str("user_id", auctionItem.AuctionBidUserID).
+						Str("cost", auctionItem.AuctionBidPrice).
+						Err(err).
+						Msg("Failed to process transaction for Purchase Sale Item.")
+					continue
+				}
+
+				// Transfer ownership of asset
+				err = db.ChangeMechOwner(itemSaleID)
+				if err != nil {
+					gamelog.L.Error().
+						Str("item_id", auctionItem.ID).
+						Str("user_id", auctionItem.AuctionBidUserID).
+						Str("cost", auctionItem.AuctionBidPrice).
+						Err(err).
+						Msg("Failed to Transfer Mech to New Owner")
+					continue
+				}
 			}
 
 			gamelog.L.Info().Msg("processing completed auction items completed")
