3a9c61fbb8183482bc02322e07bf7ca710d3a599
Yong Tan <ultimatoonmaster@gmail.com>
2022-06-08 17:47:52 +0800

    Grab collection item details

--- server/marketplace.go
+++ server/marketplace.go
@@ -34,6 +34,7 @@ type MarketplaceSaleItem struct {
 	Owner                MarketplaceSaleItemOwner        `json:"owner,omitempty" boil:",bind"`
 	Mech                 MarketplaceSaleItemMech         `json:"mech,omitempty" boil:",bind"`
 	MysteryCrate         MarketplaceSaleItemMysteryCrate `json:"mystery_crate,omitempty" boil:",bind"`
+	CollectionItem       MarketplaceSaleCollectionItem   `json:"collection_item,omitempty" boil:",bind"`
 	LastBid              MarketplaceBidder               `json:"last_bid,omitempty" boil:",bind"`
 }
 
@@ -61,16 +62,33 @@ type MarketplaceSaleItemOwner struct {
 	Gid           int         `json:"gid" boil:"players.gid"`
 }
 
+type MarketplaceSaleCollectionItem struct {
+	Tier             null.String `json:"tier,omitempty" boil:"collection_items.tier"`
+	ImageURL         null.String `json:"image_url,omitempty" boil:"collection_items.image_url"`
+	CardAnimationURL null.String `json:"card_animation,omitempty" boil:"collection_items.card_animation_url"`
+	AvatarURL        null.String `json:"avatar_url,omitempty" boil:"collection_items.avatar_url"`
+	LargeImageURL    null.String `json:"large_image_url,omitempty" boil:"collection_items.large_image_url"`
+	BackgroundColor  null.String `json:"background_color,omitempty" boil:"collection_items.background_color"`
+	YoutubeURL       null.String `json:"youtube_url,omitempty" boil:"collection_items.youtube_url"`
+}
+
+func (b MarketplaceSaleCollectionItem) MarshalJSON() ([]byte, error) {
+	if !b.Tier.Valid && !b.ImageURL.Valid && !b.CardAnimationURL.Valid && !b.AvatarURL.Valid && !b.LargeImageURL.Valid && !b.BackgroundColor.Valid && !b.YoutubeURL.Valid {
+		return null.NullBytes, nil
+	}
+	type localMarketplaceSaleCollectionItem MarketplaceSaleCollectionItem
+	return json.Marshal(localMarketplaceSaleCollectionItem(b))
+}
+
 type MarketplaceSaleItemMech struct {
 	ID        null.String `json:"id" boil:"mechs.id"`
 	Label     null.String `json:"label" boil:"mechs.label"`
 	Name      null.String `json:"name" boil:"mechs.name"`
-	Tier      null.String `json:"tier" boil:"collection_items.tier"`
 	AvatarURL null.String `json:"avatar_url" boil:"mech_skin.avatar_url"`
 }
 
 func (b MarketplaceSaleItemMech) MarshalJSON() ([]byte, error) {
-	if !b.ID.Valid && !b.Label.Valid && !b.Name.Valid && !b.Tier.Valid && !b.AvatarURL.Valid {
+	if !b.ID.Valid && !b.Label.Valid && !b.Name.Valid && !b.AvatarURL.Valid {
 		return null.NullBytes, nil
 	}
 	type localMarketplaceSaleItemMech MarketplaceSaleItemMech
@@ -78,14 +96,12 @@ func (b MarketplaceSaleItemMech) MarshalJSON() ([]byte, error) {
 }
 
 type MarketplaceSaleItemMysteryCrate struct {
-	ID          null.String `json:"id" boil:"mystery_crate.id"`
-	Label       null.String `json:"label" boil:"mystery_crate.label"`
-	ImageURL    null.String `json:"image_url" boil:"mystery_crate.image_url"`
-	LockedUntil null.Time   `json:"locked_until" boil:"mystery_crate.locked_until"`
+	ID    null.String `json:"id" boil:"mystery_crate.id"`
+	Label null.String `json:"label" boil:"mystery_crate.label"`
 }
 
 func (b MarketplaceSaleItemMysteryCrate) MarshalJSON() ([]byte, error) {
-	if !b.ID.Valid && !b.Label.Valid && !b.ImageURL && !b.LockedUntil.Valid {
+	if !b.ID.Valid && !b.Label.Valid {
 		return null.NullBytes, nil
 	}
 	type localMarketplaceSaleItemMysteryCrate MarketplaceSaleItemMysteryCrate
