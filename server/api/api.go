3fbb06ff364a4f862618394f10e48bffe2109307
owengiri20 <owengiri20@gmail.com>
2022-06-30 14:18:48 +0800

    fix

--- server/api/api.go
+++ server/api/api.go
@@ -263,7 +263,6 @@ func NewAPI(
 				s.WS("/multipliers", battle.HubKeyMultiplierSubscribe, server.MustSecure(battleArenaClient.MultiplierUpdate))
 				s.WS("/mystery_crates", HubKeyMysteryCrateOwnershipSubscribe, server.MustSecure(ssc.MysteryCrateOwnershipSubscribeHandler))
 				s.WS("/player_abilities", server.HubKeyPlayerAbilitiesList, server.MustSecure(pac.PlayerAbilitiesListHandler))
-				s.WS("/player_weapons", server.HubKeyPlayerWeaponsList, server.MustSecure(pac.PlayerAbilitiesListHandler))
 
 			}))
 
