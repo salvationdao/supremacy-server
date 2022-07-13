29909d9c23a05d613920a33d449e70e38f0b11da
felix t <41817193+o2bomb@users.noreply.github.com>
2022-07-13 18:11:09 +0800

    Change landmine colour to purple

--- server/db/migrations/20220713095026_line_select_airstrike.up.sql
+++ server/db/migrations/20220713095026_line_select_airstrike.up.sql
@@ -18,3 +18,24 @@ SET
     location_select_type = 'LINE_SELECT'
 WHERE
     game_client_ability_id = 0;
+
+UPDATE
+    blueprint_player_abilities
+SET
+    colour = '#8638c9'
+WHERE
+    game_client_ability_id = 11;
+
+UPDATE
+    consumed_abilities
+SET
+    colour = '#8638c9'
+WHERE
+    game_client_ability_id = 11;
+
+UPDATE
+    game_abilities
+SET
+    colour = '#8638c9'
+WHERE
+    game_client_ability_id = 11;
