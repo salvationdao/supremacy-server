64efe943335434a6c14cd8325f7d717a2ed18612
lolivise <tom790924@gmail.com>
2022-07-14 12:22:28 +0800

    fix batch message

--- server/battle/battle.go
+++ server/battle/battle.go
@@ -1474,7 +1474,7 @@ func (btl *Battle) Tick(payload []byte) {
 	}
 
 	if len(wsMessages) > 0 {
-		ws.PublishBatchMessages("/public/mech", HubKeyWarMachineStatUpdated, wsMessages)
+		ws.PublishBatchMessages("/public/mech", wsMessages)
 	}
 
 	if btl.playerAbilityManager().HasBlackoutsUpdated() {
