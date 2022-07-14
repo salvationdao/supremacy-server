fd004440d51ff080940e5c2f10422a58ca209975
Yong Tan <ultimatoonmaster@gmail.com>
2022-07-14 15:52:58 +0800

    Using availablity to handle when nexus mechs can be used

--- server/db/boiler/blueprint_mechs.go
+++ server/db/boiler/blueprint_mechs.go
@@ -23,21 +23,22 @@ import (
 
 // BlueprintMech is an object representing the database table.
 type BlueprintMech struct {
-	ID               string    `boiler:"id" boil:"id" json:"id" toml:"id" yaml:"id"`
-	BrandID          string    `boiler:"brand_id" boil:"brand_id" json:"brand_id" toml:"brand_id" yaml:"brand_id"`
-	Label            string    `boiler:"label" boil:"label" json:"label" toml:"label" yaml:"label"`
-	Slug             string    `boiler:"slug" boil:"slug" json:"slug" toml:"slug" yaml:"slug"`
-	WeaponHardpoints int       `boiler:"weapon_hardpoints" boil:"weapon_hardpoints" json:"weapon_hardpoints" toml:"weapon_hardpoints" yaml:"weapon_hardpoints"`
-	UtilitySlots     int       `boiler:"utility_slots" boil:"utility_slots" json:"utility_slots" toml:"utility_slots" yaml:"utility_slots"`
-	Speed            int       `boiler:"speed" boil:"speed" json:"speed" toml:"speed" yaml:"speed"`
-	MaxHitpoints     int       `boiler:"max_hitpoints" boil:"max_hitpoints" json:"max_hitpoints" toml:"max_hitpoints" yaml:"max_hitpoints"`
-	DeletedAt        null.Time `boiler:"deleted_at" boil:"deleted_at" json:"deleted_at,omitempty" toml:"deleted_at" yaml:"deleted_at,omitempty"`
-	UpdatedAt        time.Time `boiler:"updated_at" boil:"updated_at" json:"updated_at" toml:"updated_at" yaml:"updated_at"`
-	CreatedAt        time.Time `boiler:"created_at" boil:"created_at" json:"created_at" toml:"created_at" yaml:"created_at"`
-	ModelID          string    `boiler:"model_id" boil:"model_id" json:"model_id" toml:"model_id" yaml:"model_id"`
-	Collection       string    `boiler:"collection" boil:"collection" json:"collection" toml:"collection" yaml:"collection"`
-	PowerCoreSize    string    `boiler:"power_core_size" boil:"power_core_size" json:"power_core_size" toml:"power_core_size" yaml:"power_core_size"`
-	Tier             string    `boiler:"tier" boil:"tier" json:"tier" toml:"tier" yaml:"tier"`
+	ID               string      `boiler:"id" boil:"id" json:"id" toml:"id" yaml:"id"`
+	BrandID          string      `boiler:"brand_id" boil:"brand_id" json:"brand_id" toml:"brand_id" yaml:"brand_id"`
+	Label            string      `boiler:"label" boil:"label" json:"label" toml:"label" yaml:"label"`
+	Slug             string      `boiler:"slug" boil:"slug" json:"slug" toml:"slug" yaml:"slug"`
+	WeaponHardpoints int         `boiler:"weapon_hardpoints" boil:"weapon_hardpoints" json:"weapon_hardpoints" toml:"weapon_hardpoints" yaml:"weapon_hardpoints"`
+	UtilitySlots     int         `boiler:"utility_slots" boil:"utility_slots" json:"utility_slots" toml:"utility_slots" yaml:"utility_slots"`
+	Speed            int         `boiler:"speed" boil:"speed" json:"speed" toml:"speed" yaml:"speed"`
+	MaxHitpoints     int         `boiler:"max_hitpoints" boil:"max_hitpoints" json:"max_hitpoints" toml:"max_hitpoints" yaml:"max_hitpoints"`
+	DeletedAt        null.Time   `boiler:"deleted_at" boil:"deleted_at" json:"deleted_at,omitempty" toml:"deleted_at" yaml:"deleted_at,omitempty"`
+	UpdatedAt        time.Time   `boiler:"updated_at" boil:"updated_at" json:"updated_at" toml:"updated_at" yaml:"updated_at"`
+	CreatedAt        time.Time   `boiler:"created_at" boil:"created_at" json:"created_at" toml:"created_at" yaml:"created_at"`
+	ModelID          string      `boiler:"model_id" boil:"model_id" json:"model_id" toml:"model_id" yaml:"model_id"`
+	Collection       string      `boiler:"collection" boil:"collection" json:"collection" toml:"collection" yaml:"collection"`
+	PowerCoreSize    string      `boiler:"power_core_size" boil:"power_core_size" json:"power_core_size" toml:"power_core_size" yaml:"power_core_size"`
+	Tier             string      `boiler:"tier" boil:"tier" json:"tier" toml:"tier" yaml:"tier"`
+	AvailabilityID   null.String `boiler:"availability_id" boil:"availability_id" json:"availability_id,omitempty" toml:"availability_id" yaml:"availability_id,omitempty"`
 
 	R *blueprintMechR `boiler:"-" boil:"-" json:"-" toml:"-" yaml:"-"`
 	L blueprintMechL  `boiler:"-" boil:"-" json:"-" toml:"-" yaml:"-"`
@@ -59,6 +60,7 @@ var BlueprintMechColumns = struct {
 	Collection       string
 	PowerCoreSize    string
 	Tier             string
+	AvailabilityID   string
 }{
 	ID:               "id",
 	BrandID:          "brand_id",
@@ -75,6 +77,7 @@ var BlueprintMechColumns = struct {
 	Collection:       "collection",
 	PowerCoreSize:    "power_core_size",
 	Tier:             "tier",
+	AvailabilityID:   "availability_id",
 }
 
 var BlueprintMechTableColumns = struct {
@@ -93,6 +96,7 @@ var BlueprintMechTableColumns = struct {
 	Collection       string
 	PowerCoreSize    string
 	Tier             string
+	AvailabilityID   string
 }{
 	ID:               "blueprint_mechs.id",
 	BrandID:          "blueprint_mechs.brand_id",
@@ -109,6 +113,7 @@ var BlueprintMechTableColumns = struct {
 	Collection:       "blueprint_mechs.collection",
 	PowerCoreSize:    "blueprint_mechs.power_core_size",
 	Tier:             "blueprint_mechs.tier",
+	AvailabilityID:   "blueprint_mechs.availability_id",
 }
 
 // Generated where
@@ -129,6 +134,7 @@ var BlueprintMechWhere = struct {
 	Collection       whereHelperstring
 	PowerCoreSize    whereHelperstring
 	Tier             whereHelperstring
+	AvailabilityID   whereHelpernull_String
 }{
 	ID:               whereHelperstring{field: "\"blueprint_mechs\".\"id\""},
 	BrandID:          whereHelperstring{field: "\"blueprint_mechs\".\"brand_id\""},
@@ -145,17 +151,20 @@ var BlueprintMechWhere = struct {
 	Collection:       whereHelperstring{field: "\"blueprint_mechs\".\"collection\""},
 	PowerCoreSize:    whereHelperstring{field: "\"blueprint_mechs\".\"power_core_size\""},
 	Tier:             whereHelperstring{field: "\"blueprint_mechs\".\"tier\""},
+	AvailabilityID:   whereHelpernull_String{field: "\"blueprint_mechs\".\"availability_id\""},
 }
 
 // BlueprintMechRels is where relationship names are stored.
 var BlueprintMechRels = struct {
 	Brand                        string
 	Model                        string
+	Availability                 string
 	BlueprintChassisTemplatesOld string
 	BlueprintMechs               string
 }{
 	Brand:                        "Brand",
 	Model:                        "Model",
+	Availability:                 "Availability",
 	BlueprintChassisTemplatesOld: "BlueprintChassisTemplatesOld",
 	BlueprintMechs:               "BlueprintMechs",
 }
@@ -164,6 +173,7 @@ var BlueprintMechRels = struct {
 type blueprintMechR struct {
 	Brand                        *Brand        `boiler:"Brand" boil:"Brand" json:"Brand" toml:"Brand" yaml:"Brand"`
 	Model                        *MechModel    `boiler:"Model" boil:"Model" json:"Model" toml:"Model" yaml:"Model"`
+	Availability                 *Availability `boiler:"Availability" boil:"Availability" json:"Availability" toml:"Availability" yaml:"Availability"`
 	BlueprintChassisTemplatesOld *TemplatesOld `boiler:"BlueprintChassisTemplatesOld" boil:"BlueprintChassisTemplatesOld" json:"BlueprintChassisTemplatesOld" toml:"BlueprintChassisTemplatesOld" yaml:"BlueprintChassisTemplatesOld"`
 	BlueprintMechs               MechSlice     `boiler:"BlueprintMechs" boil:"BlueprintMechs" json:"BlueprintMechs" toml:"BlueprintMechs" yaml:"BlueprintMechs"`
 }
@@ -177,9 +187,9 @@ func (*blueprintMechR) NewStruct() *blueprintMechR {
 type blueprintMechL struct{}
 
 var (
-	blueprintMechAllColumns            = []string{"id", "brand_id", "label", "slug", "weapon_hardpoints", "utility_slots", "speed", "max_hitpoints", "deleted_at", "updated_at", "created_at", "model_id", "collection", "power_core_size", "tier"}
+	blueprintMechAllColumns            = []string{"id", "brand_id", "label", "slug", "weapon_hardpoints", "utility_slots", "speed", "max_hitpoints", "deleted_at", "updated_at", "created_at", "model_id", "collection", "power_core_size", "tier", "availability_id"}
 	blueprintMechColumnsWithoutDefault = []string{"brand_id", "label", "slug", "weapon_hardpoints", "utility_slots", "speed", "max_hitpoints", "model_id"}
-	blueprintMechColumnsWithDefault    = []string{"id", "deleted_at", "updated_at", "created_at", "collection", "power_core_size", "tier"}
+	blueprintMechColumnsWithDefault    = []string{"id", "deleted_at", "updated_at", "created_at", "collection", "power_core_size", "tier", "availability_id"}
 	blueprintMechPrimaryKeyColumns     = []string{"id"}
 	blueprintMechGeneratedColumns      = []string{}
 )
@@ -455,6 +465,20 @@ func (o *BlueprintMech) Model(mods ...qm.QueryMod) mechModelQuery {
 	return query
 }
 
+// Availability pointed to by the foreign key.
+func (o *BlueprintMech) Availability(mods ...qm.QueryMod) availabilityQuery {
+	queryMods := []qm.QueryMod{
+		qm.Where("\"id\" = ?", o.AvailabilityID),
+	}
+
+	queryMods = append(queryMods, mods...)
+
+	query := Availabilities(queryMods...)
+	queries.SetFrom(query.Query, "\"availabilities\"")
+
+	return query
+}
+
 // BlueprintChassisTemplatesOld pointed to by the foreign key.
 func (o *BlueprintMech) BlueprintChassisTemplatesOld(mods ...qm.QueryMod) templatesOldQuery {
 	queryMods := []qm.QueryMod{
@@ -693,6 +717,114 @@ func (blueprintMechL) LoadModel(e boil.Executor, singular bool, maybeBlueprintMe
 					foreign.R = &mechModelR{}
 				}
 				foreign.R.ModelBlueprintMechs = append(foreign.R.ModelBlueprintMechs, local)
+				break
+			}
+		}
+	}
+
+	return nil
+}
+
+// LoadAvailability allows an eager lookup of values, cached into the
+// loaded structs of the objects. This is for an N-1 relationship.
+func (blueprintMechL) LoadAvailability(e boil.Executor, singular bool, maybeBlueprintMech interface{}, mods queries.Applicator) error {
+	var slice []*BlueprintMech
+	var object *BlueprintMech
+
+	if singular {
+		object = maybeBlueprintMech.(*BlueprintMech)
+	} else {
+		slice = *maybeBlueprintMech.(*[]*BlueprintMech)
+	}
+
+	args := make([]interface{}, 0, 1)
+	if singular {
+		if object.R == nil {
+			object.R = &blueprintMechR{}
+		}
+		if !queries.IsNil(object.AvailabilityID) {
+			args = append(args, object.AvailabilityID)
+		}
+
+	} else {
+	Outer:
+		for _, obj := range slice {
+			if obj.R == nil {
+				obj.R = &blueprintMechR{}
+			}
+
+			for _, a := range args {
+				if queries.Equal(a, obj.AvailabilityID) {
+					continue Outer
+				}
+			}
+
+			if !queries.IsNil(obj.AvailabilityID) {
+				args = append(args, obj.AvailabilityID)
+			}
+
+		}
+	}
+
+	if len(args) == 0 {
+		return nil
+	}
+
+	query := NewQuery(
+		qm.From(`availabilities`),
+		qm.WhereIn(`availabilities.id in ?`, args...),
+	)
+	if mods != nil {
+		mods.Apply(query)
+	}
+
+	results, err := query.Query(e)
+	if err != nil {
+		return errors.Wrap(err, "failed to eager load Availability")
+	}
+
+	var resultSlice []*Availability
+	if err = queries.Bind(results, &resultSlice); err != nil {
+		return errors.Wrap(err, "failed to bind eager loaded slice Availability")
+	}
+
+	if err = results.Close(); err != nil {
+		return errors.Wrap(err, "failed to close results of eager load for availabilities")
+	}
+	if err = results.Err(); err != nil {
+		return errors.Wrap(err, "error occurred during iteration of eager loaded relations for availabilities")
+	}
+
+	if len(blueprintMechAfterSelectHooks) != 0 {
+		for _, obj := range resultSlice {
+			if err := obj.doAfterSelectHooks(e); err != nil {
+				return err
+			}
+		}
+	}
+
+	if len(resultSlice) == 0 {
+		return nil
+	}
+
+	if singular {
+		foreign := resultSlice[0]
+		object.R.Availability = foreign
+		if foreign.R == nil {
+			foreign.R = &availabilityR{}
+		}
+		foreign.R.BlueprintMechs = append(foreign.R.BlueprintMechs, object)
+		return nil
+	}
+
+	for _, local := range slice {
+		for _, foreign := range resultSlice {
+			if queries.Equal(local.AvailabilityID, foreign.ID) {
+				local.R.Availability = foreign
+				if foreign.R == nil {
+					foreign.R = &availabilityR{}
+				}
+				foreign.R.BlueprintMechs = append(foreign.R.BlueprintMechs, local)
 				break
 			}
 		}
@@ -991,6 +1123,85 @@ func (o *BlueprintMech) SetModel(exec boil.Executor, insert bool, related *MechM
 		related.R.ModelBlueprintMechs = append(related.R.ModelBlueprintMechs, o)
 	}
 
+	return nil
+}
+
+// SetAvailability of the blueprintMech to the related item.
+// Sets o.R.Availability to related.
+// Adds o to related.R.BlueprintMechs.
+func (o *BlueprintMech) SetAvailability(exec boil.Executor, insert bool, related *Availability) error {
+	var err error
+	if insert {
+		if err = related.Insert(exec, boil.Infer()); err != nil {
+			return errors.Wrap(err, "failed to insert into foreign table")
+		}
+	}
+
+	updateQuery := fmt.Sprintf(
+		"UPDATE \"blueprint_mechs\" SET %s WHERE %s",
+		strmangle.SetParamNames("\"", "\"", 1, []string{"availability_id"}),
+		strmangle.WhereClause("\"", "\"", 2, blueprintMechPrimaryKeyColumns),
+	)
+	values := []interface{}{related.ID, o.ID}
+
+	if boil.DebugMode {
+		fmt.Fprintln(boil.DebugWriter, updateQuery)
+		fmt.Fprintln(boil.DebugWriter, values)
+	}
+	if _, err = exec.Exec(updateQuery, values...); err != nil {
+		return errors.Wrap(err, "failed to update local table")
+	}
+
+	queries.Assign(&o.AvailabilityID, related.ID)
+	if o.R == nil {
+		o.R = &blueprintMechR{
+			Availability: related,
+		}
+	} else {
+		o.R.Availability = related
+	}
+
+	if related.R == nil {
+		related.R = &availabilityR{
+			BlueprintMechs: BlueprintMechSlice{o},
+		}
+	} else {
+		related.R.BlueprintMechs = append(related.R.BlueprintMechs, o)
+	}
+
+	return nil
+}
+
+// RemoveAvailability relationship.
+// Sets o.R.Availability to nil.
+// Removes o from all passed in related items' relationships struct (Optional).
+func (o *BlueprintMech) RemoveAvailability(exec boil.Executor, related *Availability) error {
+	var err error
+
+	queries.SetScanner(&o.AvailabilityID, nil)
+	if _, err = o.Update(exec, boil.Whitelist("availability_id")); err != nil {
+		return errors.Wrap(err, "failed to update local table")
+	}
+
+	if o.R != nil {
+		o.R.Availability = nil
+	}
+	if related == nil || related.R == nil {
+		return nil
+	}
+
+	for i, ri := range related.R.BlueprintMechs {
+		if queries.Equal(o.AvailabilityID, ri.AvailabilityID) {
+			continue
+		}
+
+		ln := len(related.R.BlueprintMechs)
+		if ln > 1 && i < ln-1 {
+			related.R.BlueprintMechs[i] = related.R.BlueprintMechs[ln-1]
+		}
+		related.R.BlueprintMechs = related.R.BlueprintMechs[:ln-1]
+		break
+	}
 	return nil
 }
 
