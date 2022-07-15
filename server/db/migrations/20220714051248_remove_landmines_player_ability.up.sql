f9994a98d3b53b4ab18e8a0eb9937a3aaf66d18b
vincentwillats <vincentwillats.software@gmail.com>
2022-07-15 10:21:59 +0800

    undeleted landmines

--- server/db/migrations/20220714051248_remove_landmines_player_ability.up.sql
+++ server/db/migrations/20220714051248_remove_landmines_player_ability.up.sql
@@ -2,17 +2,3 @@ ALTER TABLE
     sale_player_abilities
 ADD
     COLUMN deleted_at timestamptz;
-
-UPDATE
-    sale_player_abilities
-SET
-    deleted_at = now()
-WHERE
-    blueprint_id = (
-        SELECT
-            id
-        from
-            blueprint_player_abilities
-        WHERE
-            game_client_ability_id = 11
-    );
