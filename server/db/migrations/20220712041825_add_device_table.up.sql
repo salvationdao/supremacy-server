759ab9fc4a10f322722c1a4be688da3dddd1c577
Alicia Biggs <leishbiggs@live.com.au>
2022-07-18 08:02:32 +0800

    added token login and devices table

---
+++ server/db/migrations/20220712041825_add_device_table.up.sql
@@ -0,0 +1,8 @@
+create table devices(
+    id uuid primary key default gen_random_uuid(),
+    player_id uuid not null references players(id),
+    token text not null,
+    created_at timestamptz not null default now(),
+    updated_at timestamptz not null default now(),
+    deleted_at timestamptz
+);
