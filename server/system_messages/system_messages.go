cff74fe0d963764a4d937bd40ccad6664910ed9a
felix t <41817193+o2bomb@users.noreply.github.com>
2022-07-20 13:07:45 +0800

    Broadcast system message when mech is going to be in battle and when mech has completed a battle

--- server/system_messages/system_messages.go
+++ server/system_messages/system_messages.go
@@ -13,8 +13,16 @@ import (
 type SystemMessagingManager struct {
 }
 
+type SystemMessageType string
+
+const (
+	SystemMessageMechQueue          SystemMessageType = "MECH_QUEUE"
+	SystemMessageMechBattleComplete SystemMessageType = "MECH_BATTLE_COMPLETE"
+)
+
 type SystemMessage struct {
-	Message string `json:"message"`
+	Type    SystemMessageType `json:"type"`
+	Message string            `json:"message"`
 }
 
 func NewSystemMessagingManager() *SystemMessagingManager {
@@ -29,7 +37,22 @@ func (smm *SystemMessagingManager) BroadcastMechQueueMessage(queue []*boiler.Bat
 			continue
 		}
 		ws.PublishMessage(fmt.Sprintf("/user/%s/system_messages", q.OwnerID), server.HubKeySystemMessageSubscribe, &SystemMessage{
-			Message: fmt.Sprintf("Your mech, %s is about to enter the battle arena.", mech.Label),
+			Type:    SystemMessageMechQueue,
+			Message: fmt.Sprintf("Your mech, %s, is about to enter the battle arena.", mech.Label),
+		})
+	}
+}
+
+func (smm *SystemMessagingManager) BroadcastMechBattleCompleteMessage(queue []*boiler.BattleQueue) {
+	for _, q := range queue {
+		mech, err := q.Mech().One(gamedb.StdConn)
+		if err != nil {
+			gamelog.L.Debug().Interface("battleQueue", q).Msg("failed to find a mech associated with battle queue")
+			continue
+		}
+		ws.PublishMessage(fmt.Sprintf("/user/%s/system_messages", q.OwnerID), server.HubKeySystemMessageSubscribe, &SystemMessage{
+			Type:    SystemMessageMechBattleComplete,
+			Message: fmt.Sprintf("Your mech, %s, has just completed a battle in the arena.", mech.Label),
 		})
 	}
 }
