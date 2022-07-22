3b861a6fc5176215e24d9b2b9f8d41c3ec382380
felix t <41817193+o2bomb@users.noreply.github.com>
2022-07-22 15:33:30 +0800

    System messages list pagination

--- server/api/ws_system_messages.go
+++ server/api/ws_system_messages.go
@@ -30,17 +30,60 @@ func NewSystemMessagesController(api *API) *SystemMessagesController {
 	return smc
 }
 
+type SystemMessageListRequest struct {
+	*hub.HubCommandRequest
+	Payload struct {
+		Page     int `json:"page"`
+		PageSize int `json:"page_size"`
+	} `json:"payload"`
+}
+
+type SystemMessageListResponse struct {
+	Total          int                       `json:"total"`
+	SystemMessages boiler.SystemMessageSlice `json:"system_messages"`
+}
+
 func (smc *SystemMessagesController) SystemMessageList(ctx context.Context, user *boiler.Player, key string, payload []byte, reply ws.ReplyFunc) error {
-	sms, err := boiler.SystemMessages(
+	req := &SystemMessageListRequest{}
+	err := json.Unmarshal(payload, req)
+	if err != nil {
+		return terror.Error(err, "Invalid request received.")
+	}
+
+	offset := 0
+	pageSize := 10
+	if req.Payload.PageSize > 0 {
+		pageSize = req.Payload.PageSize
+	}
+
+	if req.Payload.Page > 0 {
+		offset = req.Payload.Page * pageSize
+	}
+
+	queryMods := []qm.QueryMod{}
+	queryMods = append(queryMods,
 		boiler.SystemMessageWhere.PlayerID.EQ(user.ID),
 		boiler.SystemMessageWhere.IsDismissed.EQ(false),
+	)
+	total, err := boiler.SystemMessages(queryMods...).Count(gamedb.StdConn)
+	if err != nil {
+		return terror.Error(err, "Failed to get fetch system messages. Please try again later.")
+	}
+
+	queryMods = append(queryMods,
 		qm.OrderBy(fmt.Sprintf("%s desc", boiler.SystemMessageColumns.SentAt)),
-	).All(gamedb.StdConn)
+		qm.Limit(pageSize),
+		qm.Offset(offset),
+	)
+	sms, err := boiler.SystemMessages(queryMods...).All(gamedb.StdConn)
 	if err != nil {
 		return terror.Error(err, "Failed to fetch system messages. Please try again later.")
 	}
 
-	reply(sms)
+	reply(&SystemMessageListResponse{
+		Total:          int(total),
+		SystemMessages: sms,
+	})
 
 	return nil
 }
