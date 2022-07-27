f48a105bf5718a0dcb9500b6313a5909a4339591
owengiri20 <owengiri20@gmail.com>
2022-07-27 11:10:55 +0800

    wip

--- server/db/migrations/20220726102018_custom_avatars.up.sql
+++ server/db/migrations/20220726102018_custom_avatars.up.sql
@@ -1,23 +1,33 @@
-CREATE TABLE hair
-(
-    id          UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    player_id   UUID        NOT NULL REFERENCES players(id),
-    face_id     UUID        NOT NULL REFERENCES players(id),
-    hair_id     UUID        NOT NULL REFERENCES players(id),
+-- CREATE TABLE hair
+-- (
+--     id          UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+--     hue         TEXT        NOT NULL,
+--     image_url   TEXT        NOT NULL,
 
-    updated_at  TIMESTAMPTZ NOT NULL DEFAULT NOW(),
-    deleted_at  TIMESTAMPTZ,
-    created_at  TIMESTAMPTZ NOT NULL DEFAULT NOW()
-);
+--     updated_at  TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+--     deleted_at  TIMESTAMPTZ,
+--     created_at  TIMESTAMPTZ NOT NULL DEFAULT NOW()
+-- );
 
-CREATE TABLE profile_custom_avatars
-(
-    id          UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    player_id   UUID        NOT NULL REFERENCES players(id),
-    face_id     UUID        NOT NULL REFERENCES players(id),
-    hair_id     UUID        NOT NULL REFERENCES players(id),
+-- CREATE TABLE faces
+-- (
+--     id          UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+--     hue         TEXT        NOT NULL,
+--     image_url   TEXT        NOT NULL,
 
-    updated_at  TIMESTAMPTZ NOT NULL DEFAULT NOW(),
-    deleted_at  TIMESTAMPTZ,
-    created_at  TIMESTAMPTZ NOT NULL DEFAULT NOW()
-);
+--     updated_at  TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+--     deleted_at  TIMESTAMPTZ,
+--     created_at  TIMESTAMPTZ NOT NULL DEFAULT NOW()
+-- );
+
+-- CREATE TABLE profile_custom_avatars
+-- (
+--     id          UUID        PRIMARY KEY DEFAULT gen_random_uuid(),
+--     player_id   UUID        NOT NULL REFERENCES players(id),
+--     face_id     UUID        NOT NULL REFERENCES players(id),
+--     hair_id     UUID        NOT NULL REFERENCES players(id),
+
+--     updated_at  TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+--     deleted_at  TIMESTAMPTZ,
+--     created_at  TIMESTAMPTZ NOT NULL DEFAULT NOW()
+-- );
