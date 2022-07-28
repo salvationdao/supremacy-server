411139aa5d1f8d4d8a7a73fcce6ef2e13f54ef20
Brendan Ragan <lordmortis@gmail.com>
2022-07-28 14:41:55 +0800

    add migration

---
+++ server/db/migrations/20220728063900_change_x_1000_blue_white_to_vintage.sql
@@ -0,0 +1,3 @@
+-- 1dc510dc-522b-421b-a49c-420a1ea48dd0 is blue white skin
+-- 729a33fd-6b87-4ea1-8a56-d415aa2ebcc6 is the vintage skin
+UPDATE mech_skin SET blueprint_id = '729a33fd-6b87-4ea1-8a56-d415aa2ebcc6' WHERE blueprint_id = '1dc510dc-522b-421b-a49c-420a1ea48dd0';
