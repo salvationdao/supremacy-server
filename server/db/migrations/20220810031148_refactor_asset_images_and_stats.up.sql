49c645b64980201980af581b8046ca6f1bf6e75d
vincentwillats <vincentwillats.software@gmail.com>
2022-08-10 19:37:25 +0800

    reordered migration

--- server/db/migrations/20220810031148_refactor_asset_images_and_stats.up.sql
+++ server/db/migrations/20220810031148_refactor_asset_images_and_stats.up.sql
@@ -1356,17 +1356,3 @@ UPDATE mystery_crate mc SET blueprint_id = (select id from storefront_mystery_cr
 
 ALTER TABLE mystery_crate
     ALTER COLUMN blueprint_id SET NOT NULL;
-
-
---  wasn't needed
--- WITH wps AS (
---     SELECT tpb.template_id, wmsc.blueprint_weapon_skin_id as blueprint_id, 'WEAPON_SKIN' as "type"
---     FROM template_blueprints tpb
---              INNER JOIN blueprint_weapons bpw ON bpw.id = tpb.blueprint_id
---              INNER JOIN weapon_models wm ON wm.id = bpw.weapon_model_id
---              INNER JOIN weapon_model_skin_compatibilities wmsc ON wmsc.weapon_model_id = wm.id
---     WHERE tpb.type = 'WEAPON')
--- INSERT INTO template_blueprints(template_id, type, blueprint_id)
--- SELECT wps.template_id, wps.type::template_item_type, wps.blueprint_id
--- FROM wps;
-
