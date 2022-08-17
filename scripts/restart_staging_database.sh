699be7631d78963c6d078d0c00918425178082bb
ivanngyb <ivanng2@hotmail.com>
2022-08-17 16:25:10 +0800

    got the right scripts this time

--- scripts/restart_staging_database.sh
+++ scripts/restart_staging_database.sh
@@ -1,7 +1,4 @@
-#!/bin/bash
-set -e
-
-read -p "Are you sure you want to reset staging data on Xsyn? (y/n)" -n 1 -r yn
+read -p "Are you sure you want to reset staging data on Gameserver? (y/n)" -n 1 -r yn
 case "$yn" in
     [yY] )  echo ""
             echo "Proceeding to reset data"
@@ -31,8 +28,8 @@ echo ""
 echo "What comp number is this (1,2,3, etc) : "
 read comp
 date=$(date +'%Y-%m-%d-%H%M%S')
-psql -U postgres -c "ALTER DATABASE \"passport-db\" RENAME TO \"passport-db-COMP$comp-$date\""
-psql -U postgres -c "CREATE DATABASE \"passport-db\" WITH TEMPLATE \"passport-db-POST-SETUP-2022-08-05\""
-psql -U postgres -d "passport-db" -c "ALTER DEFAULT PRIVILEGES IN SCHEMA \"public\" GRANT SELECT ON TABLES TO \"dev-readonly\";"
-psql -U postgres -d "passport-db" -c "ALTER DEFAULT PRIVILEGES IN SCHEMA \"public\" GRANT ALL PRIVILEGES ON TABLES TO \"passport\";"
-echo "DONE RESETTING XSYN"
+psql -U postgres -c "ALTER DATABASE \"gameserver-db\" RENAME TO \"gameserver-db-COMP$comp-$date\""
+psql -U postgres -c "CREATE DATABASE \"gameserver-db\" WITH TEMPLATE \"gameserver-db-POST-SETUP-2022-08-05\""
+psql -U postgres -d "gameserver-db" -c "ALTER DEFAULT PRIVILEGES IN SCHEMA \"public\" GRANT SELECT ON TABLES TO \"dev-readonly\";"
+psql -U postgres -d "gameserver-db" -c "ALTER DEFAULT PRIVILEGES IN SCHEMA \"public\" GRANT ALL PRIVILEGES ON TABLES TO \"gameserver\";"
+echo "DONE RESETTING GAMESERVER"
