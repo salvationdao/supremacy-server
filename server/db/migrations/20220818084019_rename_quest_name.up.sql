ca035edf3fd3c823e80ce5888fb2e9cad2743d09
lolivise <tom790924@gmail.com>
2022-08-18 23:38:47 +0800

    change mini mech to support machine

--- server/db/migrations/20220818084019_rename_quest_name.up.sql
+++ server/db/migrations/20220818084019_rename_quest_name.up.sql
@@ -1,1 +1,3 @@
-UPDATE quest_events SET name = 'Daily Challenge' WHERE name = 'Daily Quest'
+UPDATE quest_events SET name = 'Daily Challenge' WHERE name = 'Daily Quest';
+
+update blueprint_player_abilities SET label = 'Support Machine' WHERE game_client_ability_id = 18;
