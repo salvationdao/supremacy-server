12e253e52643fc8ce4fd1e792ee72a8c9842c735
lolivise <tom790924@gmail.com>
2022-08-30 15:12:21 +0800

    fix query

--- server/db/repair.go
+++ server/db/repair.go
@@ -1,7 +1,9 @@
 package db
 
 import (
+	"fmt"
 	"github.com/ninja-software/terror/v2"
+	"github.com/volatiletech/sqlboiler/v4/queries/qm"
 	"server"
 	"server/db/boiler"
 	"server/gamedb"
@@ -21,15 +23,15 @@ func RepairOfferDetail(offerID string) (*server.RepairOffer, error) {
 		    ro.finished_reason,
 		    rc.blocks_required_repair,
 		    rc.blocks_repaired,
-		    ro.offered_sups_amount/ro.blocks_total as sups_worth_per_block,
+		    ro.offered_sups_amount/ro.blocks_total AS sups_worth_per_block,
 		    p.username,
 		    p.gid,
 		    p.faction_id,
-		    COUNT(ra.id) as working_agent_count
+		    COUNT(ra.id) AS working_agent_count
 		FROM repair_offers ro
-		INNER JOIN repair_cases rc on rc.id = ro.repair_case_id
-		INNER JOIN players p on p.id = ro.offered_by_id
-		LEFT JOIN repair_agents ra on ra.repair_offer_id = ro.id AND ra.finished_at ISNULL
+		INNER JOIN repair_cases rc ON rc.id = ro.repair_case_id
+		INNER JOIN players p ON p.id = ro.offered_by_id
+		LEFT JOIN repair_agents ra ON ra.repair_offer_id = ro.id AND ra.finished_at ISNULL
 		WHERE ro.id = $1
 		GROUP BY ro.id, rc.blocks_required_repair, rc.blocks_repaired, ro.offered_sups_amount, ro.blocks_total, ro.closed_at, ro.finished_reason,p.username,p.gid,p.faction_id
 	`
@@ -69,7 +71,7 @@ func AbandonRepairAgent(repairAgentID string) (string, error) {
 		UPDATE
 			repair_agents
 		SET
-		    finished_at = now(),
+		    finished_at = NOW(),
 		    finished_reason = $2
 		WHERE
 		    finished_at ISNULL AND id = $1
@@ -89,8 +91,8 @@ func IsRepairCaseOwner(caseID string, playerID string) (bool, error) {
 	q := `
 		SELECT 
 			COALESCE(
-			    (SELECT true FROM collection_items ci WHERE ci.item_id = rc.mech_id AND ci.owner_id = $2), 
-			    false
+			    (SELECT TRUE FROM collection_items ci WHERE ci.item_id = rc.mech_id AND ci.owner_id = $2), 
+			    FALSE
 			)    
 		FROM repair_cases rc
 		WHERE rc.id = $1
@@ -107,17 +109,22 @@ func IsRepairCaseOwner(caseID string, playerID string) (bool, error) {
 
 func TotalRepairBlocks(mechID string) int {
 	totalRepairBlocks := GetIntWithDefault(KeyDefaultRepairBlocks, 5)
-	q := `
-		SELECT mm.repair_blocks FROM mech_models mm
-		INNER JOIN blueprint_mechs bm ON bm.model_id = mm.id
-		INNER JOIN mechs m ON m.blueprint_id = bm.id AND m.id = $1; 
-	`
-
-	err := gamedb.StdConn.QueryRow(q, mechID).Scan(&totalRepairBlocks)
+	bm, err := boiler.BlueprintMechs(
+		qm.InnerJoin(
+			fmt.Sprintf(
+				"%s ON %s = %s AND %s = ?",
+				boiler.TableNames.Mechs,
+				qm.Rels(boiler.TableNames.Mechs, boiler.MechColumns.BlueprintID),
+				qm.Rels(boiler.TableNames.BlueprintMechs, boiler.BlueprintMechColumns.ID),
+				qm.Rels(boiler.TableNames.Mechs, boiler.MechColumns.ID),
+			),
+			mechID,
+		),
+	).One(gamedb.StdConn)
 	if err != nil {
 		gamelog.L.Error().Err(err).Msg("Failed to load total repair blocks")
 		return totalRepairBlocks
 	}
 
-	return totalRepairBlocks
+	return bm.RepairBlocks
 }
