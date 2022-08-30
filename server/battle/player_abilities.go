4a71e257dbbcdec3ee49d5569695833e9ab88c92
lolivise <tom790924@gmail.com>
2022-08-30 16:06:10 +0800

    disable disable cell check

--- server/battle/player_abilities.go
+++ server/battle/player_abilities.go
@@ -5,7 +5,6 @@ import (
 	"database/sql"
 	"encoding/json"
 	"fmt"
-	"github.com/shopspring/decimal"
 	"server"
 	"server/battle/player_abilities"
 	"server/db"
@@ -1031,18 +1030,18 @@ func (arena *Arena) MechMoveCommandCreateHandler(ctx context.Context, user *boil
 		return terror.Error(err, err.Error())
 	}
 
-	// check cell is disabled or not
-	disableCells := arena.currentDisableCells()
-	if disableCells == nil {
-		return terror.Error(fmt.Errorf("no disabeld cells provided"), "The selected cell is disabled.")
-	}
-
-	selectedCell := req.Payload.StartCoords.X.Add(req.Payload.StartCoords.Y.Mul(decimal.NewFromInt(int64(arena.CurrentBattle().gameMap.CellsX)))).IntPart()
-	for _, dc := range disableCells {
-		if dc == selectedCell {
-			return terror.Error(fmt.Errorf("cell disabled"), "The selected cell is disabled.")
-		}
-	}
+	//// check cell is disabled or not
+	//disableCells := arena.currentDisableCells()
+	//if disableCells == nil {
+	//	return terror.Error(fmt.Errorf("no disabeld cells provided"), "The selected cell is disabled.")
+	//}
+	//
+	//selectedCell := req.Payload.StartCoords.X.Add(req.Payload.StartCoords.Y.Mul(decimal.NewFromInt(int64(arena.CurrentBattle().gameMap.CellsX)))).IntPart()
+	//for _, dc := range disableCells {
+	//	if dc == selectedCell {
+	//		return terror.Error(fmt.Errorf("cell disabled"), "The selected cell is disabled.")
+	//	}
+	//}
 
 	// Only perform mech move command db checks if war machine is not a mini mech
 	isMiniMech := wm.AIType != nil && *wm.AIType == MiniMech
