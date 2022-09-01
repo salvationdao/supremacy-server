747749fc868cc4aa6bee006c08532e648d9ea670
felix t <41817193+o2bomb@users.noreply.github.com>
2022-09-01 11:55:48 +0800

    Keep moving pending mechs until there are none left in the backlog on mech deploy

--- server/battle_queue/battle_queue.go
+++ server/battle_queue/battle_queue.go
@@ -33,7 +33,7 @@ type BattleQueueManager struct {
 func NewBattleQueueSystem() *BattleQueueManager {
 	qs := &BattleQueueManager{
 		blacklistedOwnerIDs:        []string{},
-		QueueTickerIntervalSeconds: db.GetIntWithDefault(db.KeyQueueTickerIntervalSeconds, 5),
+		QueueTickerIntervalSeconds: db.GetIntWithDefault(db.KeyQueueTickerIntervalSeconds, 10),
 	}
 
 	go qs.BattleQueueUpdater()
@@ -55,6 +55,66 @@ func (qs *BattleQueueManager) SetMechOwnerBlacklist(newBlacklist []string) {
 	qs.blacklistedOwnerIDs = newBlacklist
 }
 
+func (qs *BattleQueueManager) MovePendingMechs(factionID string, limit int) {
+	l := gamelog.L.With().Str("func", "MovePendingMechs").Logger()
+
+	qs.RLock()
+	// Get mechs from backlog
+	pendingMechs, err := db.GetPendingMechsFromFactionID(factionID, qs.blacklistedOwnerIDs, limit)
+	if err != nil {
+		l.Error().Err(err).Msg("Failed to fetch pending backlogged mechs")
+		return
+	}
+	qs.RUnlock()
+
+	if len(pendingMechs) < limit {
+		return
+	}
+
+	tx, err := gamedb.StdConn.Begin()
+	if err != nil {
+		l.Error().Err(err).Msg("failed to create db transaction")
+		return
+	}
+	defer tx.Rollback()
+
+	// Remove the mechs from the backlog and place them in the battle queue
+	for _, pm := range pendingMechs {
+		if pm == nil {
+			continue
+		}
+
+		bq := boiler.BattleQueue{
+			MechID:             pm.MechID,
+			FactionID:          pm.FactionID,
+			OwnerID:            pm.OwnerID,
+			FeeID:              pm.FeeID,
+			QueueFeeTXID:       pm.QueueFeeTXID,
+			QueueFeeTXIDRefund: pm.QueueFeeTXIDRefund,
+			UpdatedAt:          pm.UpdatedAt,
+			QueuedAt:           pm.QueuedAt,
+		}
+		err := bq.Insert(tx, boil.Infer())
+		if err != nil {
+			l.Error().Err(err).Msg("failed to insert into battle queue")
+			return
+		}
+
+		pm.Delete(tx)
+		if err != nil {
+			l.Error().Err(err).Msg("failed to remove from battle queue backlog")
+			return
+		}
+	}
+	err = tx.Commit()
+	if err != nil {
+		l.Error().Err(err).Msg("failed to commit db transaction")
+		return
+	}
+
+	qs.MovePendingMechs(factionID, db.FACTION_MECH_LIMIT)
+}
+
 func (qs *BattleQueueManager) BattleQueueUpdater() {
 	l := gamelog.L.With().Str("func", "BattleQueueUpdater").Logger()
 
@@ -69,11 +129,11 @@ func (qs *BattleQueueManager) BattleQueueUpdater() {
 		select {
 		case <-queueTicker.C:
 			gamelog.L.Debug().Msg("moving entries from queue backlog to battle queue")
-			// Every 5 seconds (set by KV), check whether the battle queue has enough entries to start a
+			// Every 10 seconds (set by KV), check whether the battle queue has enough entries to start a
 			// battle. If not, then pause the battle start
 
 		case deploy := <-qs.Deploy:
-			// On mech deploy, move exactly 3 entries (per faction) from the battle_queue_backlog to the
+			// On mech deploy, move entries from the battle_queue_backlog to the
 			// battle_queue table, only if they satisfy the following criteria:
 			// - No two mechs can belong to the same owner, unless the faction's battle_queue is empty
 			// - Mechs did not participate in the previous battle (ignoring server resets)
@@ -89,61 +149,7 @@ func (qs *BattleQueueManager) BattleQueueUpdater() {
 				limit = int(db.FACTION_MECH_LIMIT - (inQueueCount % db.FACTION_MECH_LIMIT))
 			}
 
-			qs.RLock()
-			// Get mechs from backlog
-			pendingMechs, err := db.GetPendingMechsFromFactionID(deploy.FactionID, qs.blacklistedOwnerIDs, limit)
-			if err != nil {
-				l.Error().Err(err).Msg("Failed to fetch pending backlogged mechs")
-				continue
-			}
-			qs.RUnlock()
-
-			if len(pendingMechs) < limit {
-				continue
-			}
-
-			func() {
-				tx, err := gamedb.StdConn.Begin()
-				if err != nil {
-					l.Error().Err(err).Msg("failed to create db transaction")
-					return
-				}
-				defer tx.Rollback()
-
-				// Remove the mechs from the backlog and place them in the battle queue
-				for _, pm := range pendingMechs {
-					if pm == nil {
-						continue
-					}
-
-					bq := boiler.BattleQueue{
-						MechID:             pm.MechID,
-						FactionID:          pm.FactionID,
-						OwnerID:            pm.OwnerID,
-						FeeID:              pm.FeeID,
-						QueueFeeTXID:       pm.QueueFeeTXID,
-						QueueFeeTXIDRefund: pm.QueueFeeTXIDRefund,
-						UpdatedAt:          pm.UpdatedAt,
-						QueuedAt:           pm.QueuedAt,
-					}
-					err := bq.Insert(tx, boil.Infer())
-					if err != nil {
-						l.Error().Err(err).Msg("failed to insert into battle queue")
-						return
-					}
-
-					pm.Delete(tx)
-					if err != nil {
-						l.Error().Err(err).Msg("failed to remove from battle queue backlog")
-						return
-					}
-				}
-				err = tx.Commit()
-				if err != nil {
-					l.Error().Err(err).Msg("failed to commit db transaction")
-					return
-				}
-			}()
+			qs.MovePendingMechs(deploy.FactionID, limit)
 		}
 	}
 }
