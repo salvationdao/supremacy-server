e701869f4cfe655421dfdb03033a1664e5848a4b
alyssa <aruefenacht14@gmail.com>
2022-09-01 11:54:54 +0800

    fixed db reset

--- server/db/migrations/20220829035041_optInImprovements.up.sql
+++ server/db/migrations/20220829035041_optInImprovements.up.sql
@@ -1,7 +1,7 @@
 INSERT INTO kv (key, value)
 VALUES ('battle_ability_location_select_duration', 20)
-ON CONFLICT DO UPDATE SET value = excluded.value;
+ON CONFLICT (key) DO UPDATE SET value = excluded.value;
 
 INSERT INTO kv (key, value)
 VALUES ('battle_ability_bribe_duration', 20)
-ON CONFLICT DO UPDATE SET value = excluded.value;
+ON CONFLICT (key) DO UPDATE SET value = excluded.value;
