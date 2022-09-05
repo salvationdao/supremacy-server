271ca0e6704cbfc61cbfd6b89270f78f0aa0728f
ivanngyb <ivanng2@hotmail.com>
2022-09-05 11:02:24 +0800

    Fix search query

--- server/db/replay.go
+++ server/db/replay.go
@@ -2,13 +2,11 @@ package db
 
 import (
 	"fmt"
+	"github.com/ninja-software/terror/v2"
+	"github.com/volatiletech/sqlboiler/v4/queries/qm"
 	"server"
 	"server/db/boiler"
 	"server/gamedb"
-	"strconv"
-
-	"github.com/ninja-software/terror/v2"
-	"github.com/volatiletech/sqlboiler/v4/queries/qm"
 )
 
 func ReplayList(
@@ -18,40 +16,44 @@ func ReplayList(
 	Offset int,
 ) (int64, []*server.BattleReplay, error) {
 	var queryMods []qm.QueryMod
-
 	queryMods = append(queryMods,
 		qm.Select(
-			boiler.BattleReplayColumns.ID,
+			qm.Rels(boiler.TableNames.BattleReplays, boiler.BattleReplayColumns.ID),
+			qm.Rels(boiler.TableNames.BattleReplays, boiler.BattleReplayColumns.BattleID),
 			boiler.BattleReplayColumns.BattleID,
-			boiler.BattleReplayColumns.IsCompleteBattle,
-			boiler.BattleReplayColumns.StreamID,
-			boiler.BattleReplayColumns.ArenaID,
-			boiler.BattleReplayColumns.RecordingStatus,
-			boiler.BattleReplayColumns.CreatedAt,
-			boiler.BattleReplayColumns.StoppedAt,
-			boiler.BattleReplayColumns.StartedAt,
+			qm.Rels(boiler.TableNames.BattleReplays, boiler.BattleReplayColumns.IsCompleteBattle),
+			qm.Rels(boiler.TableNames.BattleReplays, boiler.BattleReplayColumns.StreamID),
+			qm.Rels(boiler.TableNames.BattleReplays, boiler.BattleReplayColumns.ArenaID),
+			fmt.Sprintf("%s as arena_id", qm.Rels(boiler.TableNames.BattleReplays, boiler.BattleReplayColumns.ArenaID)),
+			qm.Rels(boiler.TableNames.BattleReplays, boiler.BattleReplayColumns.RecordingStatus),
+			qm.Rels(boiler.TableNames.BattleReplays, boiler.BattleReplayColumns.CreatedAt),
+			qm.Rels(boiler.TableNames.BattleReplays, boiler.BattleReplayColumns.StoppedAt),
+			qm.Rels(boiler.TableNames.BattleReplays, boiler.BattleReplayColumns.StartedAt),
 		),
 		boiler.BattleReplayWhere.IsCompleteBattle.EQ(true),
 		boiler.BattleReplayWhere.StreamID.IsNotNull(),
 	)
 
 	if Search != "" {
-		// check if battle number is a valid number
-		number, err := strconv.Atoi(Search)
-		if err != nil {
-			return 0, nil, terror.Error(err, "Failed to get battle number from search. Please ensure you use a real number")
-		}
-
 		queryMods = append(queryMods,
 			qm.InnerJoin(
 				fmt.Sprintf(
-					"%s ON %s = %s AND %s = ?",
-					boiler.TableNames.BattleReplays,
+					"%s ON %s = %s",
+					boiler.TableNames.Battles,
 					qm.Rels(boiler.TableNames.Battles, boiler.BattleColumns.ID),
 					qm.Rels(boiler.TableNames.BattleReplays, boiler.BattleReplayColumns.BattleID),
+				),
+			),
+			qm.Where(
+				fmt.Sprintf(
+					"EXISTS (SELECT 1 FROM %s WHERE %s = %s AND %s||%s::TEXT ILIKE ?)",
+					boiler.TableNames.GameMaps,
+					qm.Rels(boiler.TableNames.Battles, boiler.BattleColumns.GameMapID),
+					qm.Rels(boiler.TableNames.GameMaps, boiler.GameMapColumns.ID),
+					qm.Rels(boiler.TableNames.GameMaps, boiler.GameMapColumns.Name),
 					qm.Rels(boiler.TableNames.Battles, boiler.BattleColumns.BattleNumber),
 				),
-				number,
+				"%"+Search+"%",
 			),
 		)
 	}
@@ -66,10 +68,8 @@ func ReplayList(
 	}
 
 	queryMods = append(queryMods, qm.Limit(Limit))
-	fmt.Println(Limit, Offset)
 	queryMods = append(queryMods, qm.Offset(Offset))
 	queryMods = append(queryMods,
-		qm.Load(boiler.BattleReplayRels.Battle),
 		qm.Load(qm.Rels(boiler.BattleReplayRels.Battle, boiler.BattleRels.GameMap)),
 	)
 	queryMods = append(queryMods,
