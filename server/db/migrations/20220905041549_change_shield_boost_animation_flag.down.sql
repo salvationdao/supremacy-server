0e8d9fc364361cd4085d58d4040ee4fa8c394503
lolivise <tom790924@gmail.com>
2022-09-05 16:37:21 +0800

    change migration down

--- server/db/migrations/20220905041549_change_shield_boost_animation_flag.down.sql
+++ server/db/migrations/20220905041549_change_shield_boost_animation_flag.down.sql
@@ -1,7 +1,7 @@
 UPDATE
     blueprint_player_abilities
 SET
-    display_on_mini_map = true,
+    display_on_mini_map = false,
     mini_map_display_effect_type = 'NONE'
 WHERE
     game_client_ability_id = 10; -- Shield Overdrive
