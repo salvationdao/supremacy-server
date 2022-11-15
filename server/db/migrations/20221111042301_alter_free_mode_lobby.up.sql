8a0e8a6ac6c29dda68c91b74d0c7ab2b5e40cd60
lolivise <tom790924@gmail.com>
2022-11-15 17:19:18 +0800

    remove free mode lobby

--- server/db/migrations/20221111042301_alter_free_mode_lobby.up.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE battle_lobbies
-    ADD COLUMN is_free_team_mode BOOLEAN NOT NULL DEFAULT FALSE;
-
-ALTER TABLE battle_lobbies_mechs
-    ADD COLUMN team_number INT NOT NULL DEFAULT 0 CHECK(team_number <= 3); -- team number of battle lobby mech
